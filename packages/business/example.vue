f7f8e1a669a1cbdea2e570d5e0c748c1c5b04cb6
OuyangFan <fan.ouyang@transwarp.io>
2022-12-15 17:19:27 +0800

    feat: build module

--- packages/business/example.vue
+++ packages/business/example.vue
@@ -1,9 +1,11 @@
 <template>
   <h1>{{title}}</h1>
+  <Counter />
 </template>
 
 <script setup lang="ts">
   import {ref} from "vue";
+  import {Counter} from "./src";
 
   const title = ref('business');
 </script>
---
+++ packages/components/src/counter/counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="my-biz-counter">
+    <div class="num-value">{{ num }}</div>
+    <button @click="onClick">点击减1</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+const num = ref<number>(100);
+const onClick = () => {
+  num.value -= 1;
+};
+</script>
